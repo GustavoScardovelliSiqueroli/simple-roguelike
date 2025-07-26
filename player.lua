cfb1bd643808693e851a2c4b60003d7c95ade86e
GustavoScardovelliSiqueroli <gutavosecardovelli@gmail.com>
2025-07-26 13:36:25 -0300

    feat: player movement

--- player.lua
+++ player.lua
@@ -5,47 +5,40 @@ function Player:new(x, y)
 	self = setmetatable({}, Player)
 	self.x = x
 	self.y = y
-	self.velocity = 200
+	self.speed = 200
 	return self
 end
 
 function Player:update(dt)
-	if love.keyboard.isDown("w") and love.keyboard.isDown("d") then
-		self.y = self.y - self.velocity * dt / 2
-		self.x = self.x + self.velocity * dt / 2
-		return
-	end
-	if love.keyboard.isDown("w") and love.keyboard.isDown("a") then
-		self.y = self.y - self.velocity * dt / 2
-		self.x = self.x - self.velocity * dt / 2
-		return
-	end
-	if love.keyboard.isDown("s") and love.keyboard.isDown("d") then
-		self.y = self.y + self.velocity * dt / 2
-		self.x = self.x + self.velocity * dt / 2
-		return
-	end
-	if love.keyboard.isDown("s") and love.keyboard.isDown("a") then
-		self.y = self.y + self.velocity * dt / 2
-		self.x = self.x - self.velocity * dt / 2
-		return
-	end
-	if love.keyboard.isDown("w") then
-		self.y = self.y - self.velocity * dt
-	end
-	if love.keyboard.isDown("s") then
-		self.y = self.y + self.velocity * dt
-	end
-	if love.keyboard.isDown("a") then
-		self.x = self.x - self.velocity * dt
-	end
-	if love.keyboard.isDown("d") then
-		self.x = self.x + self.velocity * dt
-	end
+	local ix, iy = make_movement_vector()
+	self.x = self.x + ix * self.speed * dt
+	self.y = self.y + iy * self.speed * dt
 end
 
 function Player:draw()
 	love.graphics.rectangle("fill", self.x, self.y, 10, 10)
+end
+
+function make_movement_vector()
+	local x = 0
+	local y = 0
+	if love.keyboard.isDown("a") then
+		x = -1
+	end
+	if love.keyboard.isDown("d") then
+		x = 1
+	end
+	if love.keyboard.isDown("w") then
+		y = -1
+	end
+	if love.keyboard.isDown("s") then
+		y = 1
+	end
+	local magnitude = math.sqrt(x ^ 2 + y ^ 2)
+	if magnitude == 0 then
+		return 0, 0
+	end
+	return x / magnitude, y / magnitude
 end
 
 return Player
